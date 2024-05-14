73f6a0cbda596cf23dd67f031d40efdaa35140f1
Vish's PC <vishwadeep.lella@gmail.com>
2024-05-14 01:25:32 -0400

    chat UI w/ user info and chat set

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,61 @@
+import { useState } from "react"
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
+    const [emoji, setEmoji] = useState(false);
+    const [textBox, setTextBox] = useState("");
+
+
+    /*
+    activeSkinTone: "neutral"
+    emoji: "💓"
+    getImageUrl: ƒ getImageUrl(emojiStyle)
+    imageUrl: "https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/1f493.png"
+    isCustom: false
+    names: (2) ['heartbeat', 'beating heart']
+    unified: "1f493"
+    unifiedWithoutSkinTone: "1f493"
+    [[Prototype]]: Object
+    */
+    const handleEmoji = (e) => {
+        setTextBox(prev => prev + e.emoji);
+        setEmoji(false);
+    };
+
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className="chat_top">
+                <div className="chat_user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="chat_texts">
+                        <span>John Smith</span>
+                        <p>Lorem ipsum dolor, sit amet.</p>
+                    </div>
+                </div>
+                <div className="chat_icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+                </div>
+            </div>
+            <div className="chat_center"></div>
+            <div className="chat_bottom">
+                <div className="send_icons">
+                    <img src="./img.png" alt="" className="" />
+                    <img src="./camera.png" alt="" className="" />
+                    <img src="./mic.png" alt="" className="" />
+                </div>
+                <input type="text" placeholder="Type a message.." value={textBox} onChange={(e) => setTextBox(e.target.value)} />
+                <div className="chat_emoji">
+                    <img src="./emoji.png" alt="" onClick={() => setEmoji(prev => !prev)} />
+                    <div className="picker">
+                        <EmojiPicker open={emoji} onEmojiClick={handleEmoji} />
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+        </div>
     )
 }
 
