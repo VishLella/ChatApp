e71477e0a06d8b46335d681f4845a9263c4cd14d
Vish's PC <vishwadeep.lella@gmail.com>
2024-05-15 03:42:27 -0400

    added user details section

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,9 +1,72 @@
 import "./detail.css"
 
-const Details = () => {
+const Detail = () => {
     return (
-        <div className="detail">Detail</div>
+        <div className="detail">
+            <div className="detail_user">
+                <img src="./avatar.png" alt="" />
+                <h2>John Smith</h2>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="detail_info">
+                <div className="detail_option">
+                    <div className="detail_title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" className="" />
+                    </div>
+                </div>
+                <div className="detail_option">
+                    <div className="detail_title">
+                        <span>Privacy & help</span>
+                        <img src="./arrowUp.png" alt="" className="" />
+                    </div>
+                </div>
+                <div className="detail_option">
+                    <div className="detail_title">
+                        <span>Shared photos</span>
+                        <img src="./arrowDown.png" alt="" className="" />
+                    </div>
+                </div>
+                <div className="photos">
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA" alt="" />
+                            <span>photo_2024_2.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="downloadIcon" />
+                    </div>
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA" alt="" />
+                            <span>photo_2024_2.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="downloadIcon" />
+                    </div>
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA" alt="" />
+                            <span>photo_2024_2.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="downloadIcon" />
+                    </div>
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA" alt="" />
+                            <span>photo_2024_2.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="downloadIcon" />
+                    </div>
+                </div>
+                <div className="detail_option">
+                    <div className="detail_title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" className="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+            </div>
+        </div>
     )
 }
 
-export default Details
+export default Detail
